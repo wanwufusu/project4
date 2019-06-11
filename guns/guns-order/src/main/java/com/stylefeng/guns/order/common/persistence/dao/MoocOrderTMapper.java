84411c5ef5b028cd62df1ef284dbec64118073be
wanwufusu <760931928@qq.com>
2019-06-11 18:16:04 +0800

    邹睿明：获取订单信息

---
+++ guns/guns-order/src/main/java/com/stylefeng/guns/order/common/persistence/dao/MoocOrderTMapper.java
@@ -0,0 +1,23 @@
+package com.stylefeng.guns.order.common.persistence.dao;
+
+import com.stylefeng.guns.order.common.persistence.model.MoocOrder;
+import com.stylefeng.guns.order.common.persistence.model.MoocOrderT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 订单信息表 Mapper 接口
+ * </p>
+ *
+ * @author stylefeng
+ * @since 2019-06-11
+ */
+public interface MoocOrderTMapper extends BaseMapper<MoocOrder> {
+
+    List<MoocOrder> selectMoocOrderByUser(@Param("username")String userName,
+                                          @Param("offset")int offset,
+                                          @Param("pageSize")int pageSize);
+}
